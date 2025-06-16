6c8fb079121630232f53130ec1f4ee1e95f73024
KANTNOLI <zahargysev02@gmail.com>
2025-06-16 21:38:23 +0300

    v2.5.0

--- src/components/Friends/index.tsx
+++ src/components/Friends/index.tsx
@@ -1,41 +1,127 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import style from "./Friends.module.scss"
+
+interface users {
+    id: number,
+    login: string,
+    avatar: string,
+}
+
+interface friends {
+    user_id: number,
+    login: string,
+    avatar: string,
+    friend_id: number,
+    befriend: string,
+    status: string,
+}
 
 function Friends() {
     const [NeedUpdate, setNeedUpdate] = useState<boolean>(false)
-    let a = {
+    const [HaveFriends, setHaveFriends] = useState<boolean>(true)
+    const [Search, setSearch] = useState<string>("")
+
+
+    const users: users[] = [{
+        id: 1,
+        login: "user1",
+        avatar: "https://hubabuba.space/temp/cookies.jpg",
+    }, {
+        id: 2,
+        login: "user2",
+        avatar: "https://hubabuba.space/temp/cookies.jpg",
+    }, {
+        id: 3,
+        login: "user3",
+        avatar: "https://hubabuba.space/temp/cookies.jpg",
+    }, {
+        id: 4,
+        login: "user4",
+        avatar: "https://hubabuba.space/temp/cookies.jpg",
+    }]
+
+    const friends: friends[] = [{
         user_id: 1,
-        friend_id: 1,
-        befriend: "asd",
-        visible: "asd",
-        status: "asd",
+        friend_id: 2,
+        login: "login 2",
+        avatar: "https://hubabuba.space/temp/cookies.jpg",
+        befriend: "text",
+        status: "status 2",
+    }, {
+        user_id: 1,
+        friend_id: 3,
+        login: "login 3",
+        avatar: "https://hubabuba.space/temp/cookies.jpg",
+        befriend: "text",
+        status: "status 3",
+    }]
+
+
+    const [UserList, setUserList] = useState<users[] | friends[]>(friends)
+
+    const HandlerSearch = () => {
+        const frinedsSearch = friends.filter((user) => {
+            if (user.login.toLowerCase().includes(Search.toLowerCase())) {
+                return user
+            }
+        })
+
+        if (frinedsSearch.length) {
+            setUserList(frinedsSearch)
+            setHaveFriends(true)
+        } else {
+            const usersSearch = users.filter((user) => {
+                if (user.login.toLowerCase().includes(Search.toLowerCase())) {
+                    return user
+                }
+            })
+
+            setUserList(usersSearch || [])
+            setHaveFriends(false)
+        }
+
+        setNeedUpdate(true)
     }
-    const friends = ["a", "a"]
-    const searchArray = ["a", "a"]
+
+    useEffect(() => {
+
+    }, [NeedUpdate])
+
 
     return (
         <section className={style.body}>
             <div className={style.header}>
-                <input type="text" className={style.search} />
-                <button className={style.btnSearch}>поиск</button>
+                <input placeholder="Search" onChange={(event) => setSearch(event.target.value)} value={Search} type="text" className={style.search} />
+                <button onClick={() => HandlerSearch()} className={style.btnSearch}>поиск</button>
             </div>
 
             <div className={style.userList}>
-                <div className={style.userListNone}>
-                    <img className={style.userListNoneImg} src="https://hubabuba.space/temp/cookies.jpg" alt="" />
-                    <p className={style.userListNoneTitle}>KANTNOLI</p>
+                {
+                    UserList.map((user, id) => {
+                        if (HaveFriends) {
+                            return (
+                                <div key={id} className={style.userListYes}>
+                                    <img className={style.userListYesImg} src="https://hubabuba.space/temp/cookies.jpg" alt="" />
+                                    <p className={style.userListYesTitle}>{user.login} <span>Huesos 12.12.2002</span></p>
+                                    <input type="text" className={style.userRename} placeholder="set tag" />
+                                    {/* true ? style.userListYesBtn : style.userListYesOffBtn */}
+                                    <button className={style.userListYesBtn}>Сохранить</button>
+                                    <button className={style.userListYesBtn}>Удалить</button>
+                                </div>)
+                        } else {
+                            return (
+                                <div key={id} className={style.userListNone}>
+                                    <img className={style.userListNoneImg} src="https://hubabuba.space/temp/cookies.jpg" alt="" />
+                                    <p className={style.userListNoneTitle}>KANTNOLI</p>
 
-                    <button className={style.userListNoneBtn}>Добавить</button>
-                </div>
+                                    <button className={style.userListNoneBtn}>Добавить</button>
+                                </div>)
+                        }
+                    })
+                }
 
-                <div className={style.userListYes}>
-                    <img className={style.userListYesImg} src="https://hubabuba.space/temp/cookies.jpg" alt="" />
-                    <p className={style.userListYesTitle}>KANTNOLI {"Huesos"}</p>
-                    <input type="text" className={style.userRename} placeholder="set tag" />
 
-                    <button className={NeedUpdate ? style.userListYesBtn : style.userListYesOffBtn}>Сохранить</button>
-                    <button className={style.userListYesBtn}>Удалить</button>
-                </div>
+
             </div>
         </section>
     );
